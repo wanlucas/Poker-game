3527c1564736c65891baa38f671138a732bedf99
wan <wan.lucas.90@gmail.com>
2023-05-26 17:51:28 -0300

    feat(components): add Deck

---
+++ src/components/Deck.ts
@@ -0,0 +1,37 @@
+import Card, { Suit, Value } from "./Card";
+
+export default class Deck {
+  private cards: Card[];
+
+  constructor() {
+    this.cards = [];
+    this.generate();
+  }
+
+  private generate() {
+    const options: Card[] = [];
+
+    Card.suitOptions.forEach((suit: Suit) => {
+      Card.cardOptions.forEach((value: Value) => {
+        options.push(new Card({ value, suit }));
+      });
+    });
+
+    while(options.length) {
+      const randomIndex = Math.floor(Math.random() * options.length);
+      const card = options.splice(randomIndex, 1)[0];
+
+      this.cards.push(card);
+    }
+  }
+
+  public size() {
+    return this.cards.length;
+  }
+
+  public pick() {
+    return this.cards.pop();
+  }
+}
+
+console.log(new Deck());
